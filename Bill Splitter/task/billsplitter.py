a0a9fd473c208d1891e63e86ed3a03f17805914c
elena <ikonnikovaev@gmail.com>
2022-03-25 11:07:03 +0300

    Stage 3: 'lucky' feature

--- Bill Splitter/task/billsplitter.py
+++ Bill Splitter/task/billsplitter.py
@@ -1,4 +1,6 @@
 # write your code here
+import random
+
 friends = {}
 print('Enter the number of friends joining (including you):')
 n = int(input())
@@ -11,7 +13,15 @@ else:
         friends[name] = 0
     print('Enter the total bill value:')
     total_bill = float(input())
+    print('Do you want to use the "Who is lucky?" feature? '
+          'Write Yes/No:')
+    answer = input()
+    if answer == "Yes":
+        lucky = random.choice(list(friends.keys()))
+        print(f'{lucky} is the lucky one!')
+    else:
+        print('No one is going to be lucky')
     split_value = round(total_bill / n, 2)
     for name in friends:
         friends[name] = split_value
-    print(friends)
+    # print(friends)
